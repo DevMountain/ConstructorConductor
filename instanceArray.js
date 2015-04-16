b0a81dc22a906d7c01f38db5b071a650a2d88565
Matthew Ecklund <mecklund@ldschurch.org>
2015-04-15 19:39:07 -0600

    done with first 2 assignments

--- instanceArray.js
+++ instanceArray.js
@@ -14,28 +14,38 @@ var User = function(name, email, pw){
 //Create an Array called 'users' that will store all our instances of User.
 
   //code here
+  var users = [];
 
 
 //Now create and push into your users array 3 seperate instances of User using the data from above in that exact order
 
   //code here
+  users.push(new User('Tyler', 'tylermcginnis33@gmail.com', 'IloveJS'));
+  users.push(new User('Cahlan', 'cahlan@devmounta.in', 'IloveJS'));
+  users.push(new User('Lenny', 'lenny@theLenster.com', 'iLoveLentilSoup'));
 
 console.log('Tyler\'s information is ');
 //Console.log all of Tylers information
 
   //code here
+  console.log(users[0]);
 
 console.log('Lenny\'s information is ');
 //Now console.log all of Lennys information
 
   //code here
+  console.log(users[2]);;
 
 
 //Now create another instance of User using your own information and then add that to your users array.
 
   //code here
+  users.push(new User('Matt', 'matthew.g.ecklund@gmail.com', 'superSecretPassword'));
 
 console.log('All my users names are ');
 //Now loop through your users Array and console.log every users name. 
 
   //code here
+  for( var i = 0; i < users.length; i++){
+    console.log(users[i].name);
+  }
